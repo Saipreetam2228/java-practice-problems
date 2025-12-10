019273bc539648905743fe1e213b78873ea5d957
Saipreetam <saipreetam.official@gmail.com>
2025-12-10 15:53:10 +0530

    Add files via upload

---
+++ Assignment-1/1/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+  public static void main(String[] args){
+    Greeting g1 = new Greeting();
+    g1.display();
+  }
+}
+
+/*
+compilation process
+  -> javac Greeting.java
+  -> java Main
+*/ 
+
