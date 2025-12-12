6a1f1a87b08cf96642e0805cbc61a6af926b0d07
Saipreetam <saipreetam.official@gmail.com>
2025-12-12 16:11:01 +0530

    Add files via upload

---
+++ LinkedList-java-code/Collection/SinglyLinkedList.java
@@ -0,0 +1,74 @@
+package collections;
+// SinglyLinkedListNode
+public class SinglyLinkedList implements LinkedList<Integer> {
+	public SinglyLinkedList next;
+	public int data; // Integer is a wrapper class for int.
+
+	// for, while, do...while()
+	// while(predicate){}
+	// predicate is nothing but a boolean expression which evaluates to true or false
+	// for(int i=?;predicate;i++){}
+	int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	// for (int i : array) {}
+	// do...while(predicate)
+	// break; continue;
+
+
+	public void addNode(Integer data) {
+		SinglyLinkedList temp = this;
+		// this.addnode(data);
+		// this is the reference of the object that has been used to invoke "addNode()" method
+		do {
+			// && is a logical operation in java
+			// & is a bitwise operation in java
+			if(temp.next == null) {
+				SinglyLinkedList next = new SinglyLinkedList();
+				next.data = data;
+				temp.next = next;
+				break;
+			}
+			temp = temp.next;
+		} while(temp != null);
+	}
+
+        public void deleteNode(Integer target){
+          SinglyLinkedList temp = this;
+
+          if (temp.data == target && temp.next != null) {
+              temp.data = temp.next.data;
+              temp.next = temp.next.next;
+              return;
+          }
+
+          while (temp.next != null) {
+              if (temp.next.data == target) {
+                  temp.next = temp.next.next;
+                  return;
+              }
+              temp = temp.next;
+          }
+
+          System.out.println("Node with value " + target + " not found.");
+        }
+        
+        public int sizeOf(){return -1;}
+        
+        public Integer find(){return -1;}
+        
+        public void replace(Integer oldValue, Integer newValue) {
+          SinglyLinkedList temp = this;
+          boolean found = false;
+
+          while (temp != null) {
+              if (temp.data == oldValue) {
+                  temp.data = newValue;
+                  found = true;
+              }
+              temp = temp.next;
+          }
+
+          if (!found) {
+              System.out.println("Value " + oldValue + " not found to replace.");
+          }
+      }
+}
